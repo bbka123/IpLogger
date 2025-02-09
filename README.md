ccfffb0ae15c9ee7b7a1a13342102192304f13da
bbka123 <1391310991@qq.com>
2025-02-10 06:29:41 +0800

    测试

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A LeviLamina mod
 
 记录玩家IP和修改motd玩家数量
 
+#建议
+在根目录执行，以关联依赖包
+xmake project -k compile_commands
+
 ## Contributing
 
 Ask questions by creating an issue.
--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,11 +1,21 @@
 #include "mod/MyMod.h"
-
 #include "ll/api/mod/RegisterHelper.h"
+
+#include <fmt/format.h>
+#include <functional>
 
 #include <ll/api/command/Command.h>
 #include <ll/api/command/CommandHandle.h>
 #include <ll/api/command/CommandRegistrar.h>
 #include <ll/api/event/player/PlayerJoinEvent.h>
+
+#include <ll/api/service/Bedrock.h>
+#include <mc/world/actor/ActorType.h>
+#include <mc/world/actor/player/Player.h>
+#include <mc/server/commands/CommandOrigin.h>
+#include <mc/server/commands/CommandOutput.h>
+#include <mc/server/commands/CommandPermissionLevel.h>
+
 
 namespace my_mod {
 
@@ -22,7 +32,51 @@ bool MyMod::load() {
 
 bool MyMod::enable() {
     getSelf().getLogger().info("ping插件已运行...");
-    // Code for enabling the mod goes here.
+    
+
+    // Register commands.
+    auto commandRegistry = ll::service::getCommandRegistry();
+    if (!commandRegistry) {
+        throw std::runtime_error("failed to get command registry");
+    }
+
+    const auto& logger = MyMod::getInstance().getSelf().getLogger();
+    
+    logger.debug("debug 日志命令已注册");
+
+    auto& command = ll::command::CommandRegistrar::getInstance().getOrCreateCommand("suicide", "Commits suicide.", CommandPermissionLevel::Any);
+
+    command.overload().execute(
+        [&](CommandOrigin const& origin, CommandOutput& output)
+        {
+            auto* entity = origin.getEntity();
+            if (entity == nullptr || !entity->isType(ActorType::Player)) {
+                output.error("Only players can commit suicide");
+                return;
+            }
+    
+            auto* player = static_cast<Player*>(entity); // NOLINT(cppcoreguidelines-pro-type-static-cast-downcast)
+            player->kill();
+    
+            logger.info("{} killed themselves", player->getRealName());
+        });
+
+    auto& command2 = ll::command::CommandRegistrar::getInstance().getOrCreateCommand("ip", "获取IP地址", CommandPermissionLevel::Any);
+
+    command2.overload().execute(
+        [&](CommandOrigin const& origin, CommandOutput& output)
+        {
+            auto* entity = origin.getEntity();
+            if (entity == nullptr || !entity->isType(ActorType::Player)) {
+                output.error("Only players can commit suicide");
+                return;
+            }
+    
+            auto* player = static_cast<Player*>(entity); // NOLINT(cppcoreguidelines-pro-type-static-cast-downcast)
+            player->sendMessage("你的地址是: "+player->getIPAndPort());
+
+            logger.info("{} 地址: ", player->getIPAndPort());
+        });
     return true;
 }
 
